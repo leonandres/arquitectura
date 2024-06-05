b1ca2e2dfa8557a08076e3529ca814491305a325
Andrés León <andresleonb90@gmail.com>
2024-06-05 02:45:06 -0300

    Se agregan estilos al index y se crea main.js

---
+++ main.js
@@ -0,0 +1,11 @@
+let nombreEdificioAntiguo   = "Taj majal";
+let nombreEdificioEdadMedia = "Taj majal";
+let nombreEdificioModerno   = "Taj majal";
+
+let edadEdificioAntiguo = 20;
+let edadEdificioMedio   = 20;
+let edadEdificioModerno = 20;
+
+let epocaAntiguo    = 200;
+let epocaMedio      = 200;
+let epocaModerno    = 200;
