1b1a7e6c164febc651824a5f153fb486ab411f6f
Andrés León <andresleonb90@gmail.com>
2024-06-05 09:28:06 -0300

    Se agrega main.js a index.html. Cambios en estilos y se agrega sombra en styles.css

--- main.js
+++ main.js
@@ -6,6 +6,14 @@ let edadEdificioAntiguo = 20;
 let edadEdificioMedio   = 20;
 let edadEdificioModerno = 20;
 
-let epocaAntiguo    = 200;
-let epocaMedio      = 200;
-let epocaModerno    = 200;
+let epocaAntiguo = 200;
+let epocaMedio   = 200;
+let epocaModerno = 200;
+
+let rutaImagenEdificioAntiguo = "https://source.unsplash.com/300x300?building";
+let rutaImagenEdificioMedio   = "https://source.unsplash.com/300x300?building";
+let rutaImagenEdificioModerno = "https://source.unsplash.com/300x300?building";
+
+antiguo.innerHTML = "<img src = 'https://source.unsplash.com/300x300?building'>"
+medio.innerHTML = "<img src = 'https://source.unsplash.com/300x300?building'>";
+moderno.innerHTML = "<img src = 'https://source.unsplash.com/300x300?building'>";
